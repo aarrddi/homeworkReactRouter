3150fa70ae6fa38526d8bb8b07176462b94df67d
S candel <ardiandro90@gmail.com>
2023-09-02 23:59:55 +0700

    commit homework 2nd

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes , Route } from "react-router-dom"
 import Login from "./login"
+import "./App.css"
 
 function App() {
 
--- front/src/login.jsx
+++ front/src/login.jsx
@@ -5,15 +5,20 @@ import {
     FormLabel,
     Input,
     Button,
-    Center
+    Center,
+    Text,
+    Stack
         } from '@chakra-ui/react'
 import {login} from "./fetch/auth.js"
-import Swal from "sweetalert2";
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router-dom";
+
 
 
 function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const navigate = useNavigate();
 
 
 const handleSubmit = async () => {
@@ -27,6 +32,8 @@ const handleSubmit = async () => {
         showConfirmButton: false,
         timer: 1500
       })
+
+      navigate("/");
     }catch(err){
         Swal.fire({
             icon: 'error',
@@ -40,8 +47,9 @@ const handleSubmit = async () => {
 }
 return(
     <>
-    <Container>
-        <Center>
+    <Center h="100%" w="100%">
+            <Stack spacing="10px">
+            <Text fontSize='5xl' align="center">Login</Text>
         <FormControl>
             <FormLabel>Email Address</FormLabel>
             <Input type="email" onChange={(e) => setEmail(e.target.value)} />
@@ -49,8 +57,9 @@ return(
             <Input type="password" onChange={(e) => setPassword(e.target.value)} />
             <Button onClick={handleSubmit}>SUBMIT</Button>
         </FormControl>
+            </Stack>
         </Center>
-    </Container>
+    {/* </Container> */}
     </>
 )
 }
